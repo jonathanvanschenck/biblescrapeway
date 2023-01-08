6e6df10deebc50d24838cf01b08ca3db4e647c8a
Jonathan Van Schenck <jvschenck@novadynamics.com>
2023-01-07 16:11:44 -0800

    Add '--version' to CLI, and change flags

--- biblescrapeway/cli.py
+++ biblescrapeway/cli.py
@@ -4,6 +4,7 @@ import json
 from pathlib import Path
 
 from .query import query
+from .version import __version__
 
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 
@@ -22,18 +23,18 @@ def _formatter(obj_list, _format):
     return "\n".join([_format_line(obj,_format) for obj in obj_list])
 
 @click.command(context_settings=CONTEXT_SETTINGS)
-@click.option('--version', '-v', default='ESV', help='Bible version to query, default is `ESV`')
+@click.option('--translation', '-t', default='ESV', help='Bible translation to query, default is `ESV`')
 @click.option('--format', '-f', '_format', default='refstr', type=click.Choice(['refstr','str','json']),
         help='Specify output format, default is `refstr`')
 @click.option('--cache/--no-cache',  default=False, 
         help='Look up verses saved in a local cache first, and save new queries locally')
 @click.argument('reference_string')
-def scrape(version, reference_string, _format, cache):
-    """Scrape bible verses
+@click.version_option(message='%(version)s', version=__version__)
+def scrape(translation, reference_string, _format, cache):
+    """Biblescrapeway: Scrape bible verses from the web
 
     REFERENCE_STRING a (comma delimited) list of references, e.g. `John3.16` or `1Peter3.1-5` or `Gen1,2`
     """
-    
-    verses = query( reference_string, version, cache )
+    verses = query( reference_string, translation, cache )
     click.echo(_formatter(verses, _format))
 
